6418c489681b978a584be64f5ad921b4ad9dd9f1
Lucas-Veras <verasluca00@gmail.com>
2024-11-11 21:44:59 -0300

    fix: use .env port

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const port = 3000;
-
-// import config from './config/database.js';
 import app from './src/app.js';
 import { logger } from './src/config/logger.js';
+import { port } from './src/utils/port.js';
 
 app.listen(port, () => {
   logger.info(`Server está rodando em http://localhost:${port}`);
