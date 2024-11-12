fe7cd316305e5f22f30d726dc9c5405d283a746a
Lucas-Veras <verasluca00@gmail.com>
2024-11-11 21:46:35 -0300

    docs: add auth route documentation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,41 @@ import AuthPloomesController from '../controllers/authPloomesController.js';
 
 const authRoutes = Router();
 
+/**
+ * @swagger
+ * /auth:
+ *   post:
+ *     summary: Conecta com a API do CRM Ploomes
+ *     description: Autentica o usuário utilizando a API Key fornecida para acessar o CRM Ploomes.
+ *     parameters:
+ *       - in: query
+ *         name: apiKey
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: A chave de API necessária para autenticação no Ploomes CRM
+ *     responses:
+ *       200:
+ *         description: Conexão realizada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Conexão realizada com sucesso"
+ *       400:
+ *         description: Erro de requisição devido a uma chave de API ausente ou inválida
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Chave de API inválida ou ausente"
+ */
 authRoutes.post('/auth', AuthPloomesController.connect);
 
 export default authRoutes;
