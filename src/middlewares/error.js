50ffd88f81bef5b7e8582b69bddce7e2a6cea53e
Lucas-Veras <verasluca00@gmail.com>
2024-11-11 21:45:43 -0300

    fix: middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
+import { errorMessages } from '../utils/errorMessages.js';
 
 export const errorMiddleware = (err, req, res, next) => {
-  if (err) {
-    return res.status(err.statusCode).json({
-      statusCode: err.statusCode,
-      message: err.message,
-    });
-  }
-
   if (axios.isAxiosError(err) && err.response) {
     return res.status(err.response.status).json({
       statusCode: err.response.status,
-      message: 'Erro ao chamar API externa',
+      message: errorMessages[err.response.status] ?? err.response.statusText,
     });
   }
 
---
+++ src/utils/errorMessages.js
@@ -0,0 +1,4 @@
+export const errorMessages = {
+  401: 'Não autorizado',
+  404: 'Não encontrado',
+};
